187679f539f93e6158a6933298d263db7cc64a4f
Casira <casiracopes@proton.me>
2024-03-11 22:53:51 -0400

    Finished the library with unit tests for error handling

--- Week 3/library.test.ts
+++ Week 3/library.test.ts
@@ -1,8 +1,8 @@
-import { library, Book } from "./library";
+import { library, Book, newBooks } from "./library";
 //
-const book1 = new Book("American Ending", "Mary Kay Zuravleff", true);
-const book2 = new Book("Mother Land", "Leah Franqui", true);
-const book3 = new Book("The Count of Monte Cristo", "Alexandre Dumas", true);
+const book1 = new Book("American Ending", "Mary Kay Zuravleff");
+const book2 = new Book("Mother Land", "Leah Franqui");
+const book3 = new Book("The Count of Monte Cristo", "Alexandre Dumas");
 //Should add the 3 books to the library
 test("addBook", () => {
   expect(library.addBook(book1)).toBe(undefined);
@@ -22,4 +22,25 @@ test("getAvailableBooks", () => {
   console.log = jest.fn();
   expect(library.getAvailableBooks()).toBe(undefined);
   expect(console.log).toHaveBeenCalledWith(`American Ending\nMother Land`);
+});
+//
+test("receiveBooks", () => {
+  expect(library.receiveBooks(newBooks)).toBe(undefined);
+  expect(library.books).toEqual([
+    { author: "Mary Kay Zuravleff", available: true, title: "American Ending" },
+    { author: "Leah Franqui", available: true, title: "Mother Land" },
+    { author: "Alexandre Dumas", available: false, title: "The Count of Monte Cristo" },
+    { author: "Alison Kafer", title: "Feminist, Queer, Crip" },
+    { author: "Etaf Rum", title: "A Woman Is No Man" },
+    { author: "Ann Napolitano", title: "Dear Edward" },
+    { author: "Gabrielle Union", title: "You Got Anything Strong?" },
+  ]);
+});
+//
+test("Error Handling", () => {
+  console.error = jest.fn();
+  expect(library.receiveBooks(`newBoos`)).toBe(undefined);
+  expect(console.error).toHaveBeenCalledWith(
+    `Invalid JSON receipt for book order. ERROR: SyntaxError: Unexpected token e in JSON at position 1`
+  );
 });
--- Week 3/library.ts
+++ Week 3/library.ts
@@ -20,10 +20,28 @@ export const library = {
     let theBook = this.books.find((book: Book) => book.title === title);
     if (theBook && theBook.available) {
       theBook.available = false;
+    } else {
+      console.log(`Sorry, '${title}' is not in this library.`);
     }
   },
   getAvailableBooks() {
     let bookList = this.books.filter((book: Book) => book.available);
     console.log(bookList.map((book: Book) => book.title).join("\n"));
   },
+  //Learned how to parse JSON data 'as' different types! Plus try/catch error handling.
+  receiveBooks(newBooks: string) {
+    try {
+      let order = JSON.parse(newBooks) as Book[];
+      order.forEach((book: Book) => library.addBook(book));
+    } catch (error) {
+      console.error(`Invalid JSON receipt for book order. ERROR: ${error}`);
+    }
+  },
 };
+//
+export const newBooks = `[
+    {"title": "Feminist, Queer, Crip", "author": "Alison Kafer"},
+    {"title": "A Woman Is No Man", "author": "Etaf Rum"},
+    {"title": "Dear Edward", "author": "Ann Napolitano"},
+    {"title": "You Got Anything Strong?", "author": "Gabrielle Union"}
+]`;
